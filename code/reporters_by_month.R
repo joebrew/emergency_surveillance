ffaa0bf3be4d8c4520463fc3063dd69dd348b26d
joebrew <joebrew@gmail.com>
2015-10-07 16:14:12 -0400

    getting whether a hospital reported or not by time

---
+++ code/reporters_by_month.R
@@ -0,0 +1,86 @@
+#####
+# PACKAGES
+#####
+library(dplyr)
+library(readr)
+
+#####
+# DIRECTORY ISSUES
+#####
+project_root <- getwd() # Open in emergency_surveillance
+code_dir <- paste0(project_root, '/code')
+data_dir <- paste0(project_root, '/data')
+
+#####
+# READ IN DATA
+#####
+setwd(data_dir)
+data_files <- dir()
+results_list <- list()
+
+# Loop
+for (i in 1:length(data_files)){
+
+  # Update
+  cat(paste0('Working on ', i, ' of ', length(data_files)))
+  
+  
+  # Read in data for that date
+  temp <- read.table(data_files[i], 
+                     sep = ',', 
+                     header = TRUE)
+  # Clean up date
+  temp$date <- as.Date(temp$Date, '%m/%d/%Y')
+  # Get the actual date
+  temp_date <- as.Date(gsub('.txt', '', data_files[i], fixed = TRUE),
+                       format = '%d%b%Y')
+  
+  # Group by hospital and date
+  temp <- temp %>%
+    group_by(date, HospitalName) %>%
+    summarise(n = n())
+  
+  # Put temp into the results list
+  results_list[[i]] <- temp
+
+  # Update
+  cat(paste0('---done!\n'))
+  
+}
+
+#####
+# BIND TOGETHER THE RESULTS
+#####
+results <- do.call('rbind', results_list)
+
+#####
+# CLEAN UP
+#####
+# Get an expanded grid version
+eg <- expand.grid('date' = unique(sort(results$date)),
+                  'HospitalName' = unique(sort(results$HospitalName)))
+# Join eg and results, so that we have NA's for the 0s
+eg <- left_join(eg, results)
+# Make NA's 0s
+eg$n[which(is.na(eg$n))] <- 0
+# Get a month and year
+eg$month <- as.numeric(format(eg$date, '%m'))
+eg$year <- as.numeric(format(eg$date, '%Y'))
+
+# Write the csv
+setwd(data_dir)
+dir.create('jonathan_results')
+setwd('jonathan_results/')
+write_csv(eg, 'hospital_visits_over_time.csv')
+
+# Read csv and anonymize
+eg <- read_csv('hospital_visits_over_time.csv')
+
+# Make a boolean reported or not
+eg$reported <- ifelse(eg$n > 0, 'yes', 'no')
+
+# Slim down
+eg <- eg[,c('year', 'month', 'HospitalName', 'reported')]
+
+# Write
+write_csv(eg, 'hospital_visits_over_time_anonymized.csv')
