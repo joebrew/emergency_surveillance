e938b7f0ae7a797fd6c311ee11faab2c7d7a1a98
joebrew <joebrew@gmail.com>
2015-11-12 16:19:00 -0500

    minor changes

--- code/reporters_by_month.R
+++ code/reporters_by_month.R
@@ -67,14 +67,14 @@ eg <- left_join(eg, results)
 # Make 
 setwd(data_dir)
 setwd('jonathan_results/')
-write_csv(eg, 'hospital_visits_by_day.csv')
-NA's 0s
+
 eg$n[which(is.na(eg$n))] <- 0
 # Get a month and year
 eg$month <- as.numeric(format(eg$date, '%m'))
 eg$year <- as.numeric(format(eg$date, '%Y'))
 
 # Write a by day csv for Jonathan
+write_csv(eg, 'hospital_visits_by_day.csv')
 
 # Group by year-month
 eg_grouped <-
