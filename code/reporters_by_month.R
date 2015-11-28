e58faf55a7bfb8e5a915fbcf78c5739ffc46c7c3
joebrew <joebrew@gmail.com>
2015-11-27 20:03:53 -0500

    big query for jonathan with exclusion criteria and broken down by age, county of residence and day-date

--- code/reporters_by_month.R
+++ code/reporters_by_month.R
@@ -12,6 +12,24 @@ code_dir <- paste0(project_root, '/code')
 data_dir <- paste0(project_root, '/data')
 
 #####
+# DEFINE JONATHAN'S EXCLUSION CRITERIA
+#####
+bad_hospitals <- 
+  readLines('exclude.txt')
+
+exclude <- expand.grid(
+  HospitalName = as.character(c(bad_hospitals, 
+                                'Sacred Heart Hospital on the Gulf')),
+  year = 2005:2015,
+  exclude = TRUE
+)
+
+# Remove exclusion for Sacred Heart after 2009
+exclude$exclude[exclude$HospitalName == 'Sacred Heart Hospital on the Gulf' &
+                  exclude$year >= 2010] <- FALSE
+exclude <- exclude[exclude$exclude,]
+
+#####
 # READ IN DATA
 #####
 setwd(data_dir)
@@ -35,11 +53,24 @@ for (i in 1:length(data_files)){
     # Get the actual date
     temp_date <- as.Date(gsub('.txt', '', data_files[i], fixed = TRUE),
                          format = '%d%b%Y')
+    # Get year
+    temp$year <- as.numeric(format(temp$date, '%Y'))
     
-    # Group by hospital and date
+    # Get rid of exclude by joining to the exclude dataframe
+    temp <- left_join(x = temp,
+                      y = exclude)
+    temp$exclude[is.na(temp$exclude)] <- FALSE
+    temp <- temp[!temp$exclude,]
+    
+    # Group by date, age and county of residence
     temp <- temp %>%
-      group_by(date, HospitalName) %>%
-      summarise(n = n())
+      group_by(date, Age, Region) %>%
+      summarise(n = n()) %>%
+      rename(age = Age,
+             county_of_residence = Region)
+    
+    # Order by county of residence, date
+    temp <- arrange(temp, county_of_residence, date, age)
     
     # Put temp into the results list
     results_list[[i]] <- temp
@@ -61,7 +92,8 @@ save.image('~/Desktop/temp.RData')
 #####
 # Get an expanded grid version
 eg <- expand.grid('date' = unique(sort(results$date)),
-                  'HospitalName' = unique(sort(results$HospitalName)))
+                  'age' = unique(sort(results$age)),
+                  'county_of_residence' = unique(sort(results$county_of_residence)))
 # Join eg and results, so that we have NA's for the 0s
 eg <- left_join(eg, results)
 # Make 
@@ -70,32 +102,37 @@ setwd('jonathan_results/')
 
 eg$n[which(is.na(eg$n))] <- 0
 # Get a month and year
-eg$month <- as.numeric(format(eg$date, '%m'))
-eg$year <- as.numeric(format(eg$date, '%Y'))
+# eg$month <- as.numeric(format(eg$date, '%m'))
+# eg$year <- as.numeric(format(eg$date, '%Y'))
 
 # Write a by day csv for Jonathan
-write_csv(eg, 'hospital_visits_by_day.csv')
+write_csv(eg, 'hospital_visits_by_day_by_age_by_residence.csv')
 
-# Group by year-month
-eg_grouped <-
-  eg %>%
-  group_by(year, month, HospitalName) %>%
-  summarise(n = sum(n, na.rm = TRUE))
-
-# Write the csv
-setwd(data_dir)
-dir.create('jonathan_results')
-setwd('jonathan_results/')
-write_csv(eg_grouped, 'hospital_visits_over_time.csv')
-
-# Read csv and anonymize
-eg_grouped <- read_csv('hospital_visits_over_time.csv')
-
-# Make a boolean reported or not
-eg_grouped$reported <- ifelse(eg_grouped$n > 0, 'yes', 'no')
-
-# Slim down
-eg_grouped <- eg_grouped[,c('year', 'month', 'HospitalName', 'reported', 'n')]
-
-# Write
-write_csv(eg_grouped, 'hospital_visits_over_time_anonymized.csv')
+# # Group by year-month
+# # Get a month and year
+# eg$month <- as.numeric(format(eg$date, '%m'))
+# eg$year <- as.numeric(format(eg$date, '%Y'))
+# 
+# 
+# eg_grouped <-
+#   eg %>%
+#   group_by(year, month, age, county_of_residence) %>%
+#   summarise(n = sum(n, na.rm = TRUE))
+# 
+# # Write the csv
+# setwd(data_dir)
+# dir.create('jonathan_results')
+# setwd('jonathan_results/')
+# write_csv(eg_grouped, 'hospital_visits_over_time.csv')
+# 
+# # Read csv and anonymize
+# eg_grouped <- read_csv('hospital_visits_over_time.csv')
+# 
+# # Make a boolean reported or not
+# eg_grouped$reported <- ifelse(eg_grouped$n > 0, 'yes', 'no')
+# 
+# # Slim down
+# eg_grouped <- eg_grouped[,c('year', 'month', 'HospitalName', 'reported', 'n')]
+# 
+# # Write
+# write_csv(eg_grouped, 'hospital_visits_over_time_anonymized.csv')
